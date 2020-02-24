6677731a72df12307c83641ef2adb4b34577fdac
duduluu <duduluu@outlook.com>
2020-02-24 23:09:19 +0800

    ci: fix publish steps

--- packages/template-ci/index.js
+++ packages/template-ci/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: '@void-aurora/template-ci',
   description: 'Empty package for test ci',
-  timestamp: '2020-02-24T15:07:25.734Z',
+  timestamp: '2020-02-24T15:08:58.680Z',
 };
