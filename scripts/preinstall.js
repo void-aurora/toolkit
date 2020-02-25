373d81696e7855f5c0428691ea765a3f718dc22a
duduluu <duduluu@outlook.com>
2020-02-25 17:51:05 +0800

    chore: lifecycle script preinstall

---
+++ scripts/preinstall.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+
+if (!process.env.npm_config_user_agent.startsWith('pnpm')) {
+  console.log(
+    `${chalk.bgRed.black(' ERROR ')} Use \`${chalk.cyan(
+      'pnpm -r install',
+    )}\` to install dependencies in this repository`,
+  );
+  process.exit(1);
+}
