7438042f976ec56e0f45bc7ab1f7667385102a73
duduluu <duduluu@outlook.com>
2020-02-25 18:26:43 +0800

    chore(template-ci): test lerna version command

--- packages/template-ci/index.js
+++ packages/template-ci/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: '@void-aurora/template-ci',
   description: 'Empty package for test ci',
-  timestamp: '2020-02-24T15:08:58.680Z',
+  timestamp: '2020-02-25T10:26:11.189Z',
 };
