b660bffdbcfb67c70bfee95309d8a9ea5ed5f915
duduluu <duduluu@outlook.com>
2020-02-25 17:58:30 +0800

    chore: fix lifecycle script preinstall, remove chalk require

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,10 +1,4 @@
-const chalk = require('chalk');
-
 if (!process.env.npm_config_user_agent.startsWith('pnpm')) {
-  console.log(
-    `${chalk.bgRed.black(' ERROR ')} Use \`${chalk.cyan(
-      'pnpm -r install',
-    )}\` to install dependencies in this repository`,
-  );
+  console.log('Use `pnpm -r install` to install dependencies in this repository');
   process.exit(1);
 }
