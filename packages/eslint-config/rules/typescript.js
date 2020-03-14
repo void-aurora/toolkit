7735d3817d39f83e5c6fe006f3b2b0b62016fcde
duduluu <duduluu@outlook.com>
2020-03-14 20:14:28 +0800

    fix(eslint-config): disable ts/prefer-readonly-parameter-types for js

--- packages/eslint-config/rules/typescript.js
+++ packages/eslint-config/rules/typescript.js
@@ -669,6 +669,7 @@ module.exports = {
       rules: {
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/typedef': 'off',
+        '@typescript-eslint/prefer-readonly-parameter-types': 'off',
       },
     },
   ],
