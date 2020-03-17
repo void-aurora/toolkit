3dfd790181ee8273ee8bf2ec70b6bf972dcfc055
duduluu <duduluu@outlook.com>
2020-03-17 22:43:35 +0800

    feat(template-just): add template just

---
+++ packages/template-just/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * The hello world text.
+ */
+export const helloWorld = 'Hello, World!';
+
+/**
+ * Return hello world text.
+ */
+export function sayHello(): string {
+  return helloWorld;
+}
