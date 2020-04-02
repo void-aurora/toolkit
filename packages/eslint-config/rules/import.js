5bedfa51014df154085650935fc3a0003d591f2a
duduluu <duduluu@outlook.com>
2020-04-03 01:24:23 +0800

    fix(eslint-config): disable rule import/no-extraneous-dependencies for non-src scripts

--- packages/eslint-config/rules/import.js
+++ packages/eslint-config/rules/import.js
@@ -95,33 +95,7 @@ module.exports = {
     'import/no-extraneous-dependencies': [
       'error',
       {
-        devDependencies: [
-          '**/build/**',
-          '**/script/**',
-          '**/scripts/**',
-          '**/test/**', // tape, common npm pattern
-          '**/tests/**', // also common npm pattern
-          '**/spec/**', // mocha, rspec-like pattern
-          '**/__tests__/**', // jest pattern
-          '**/__mocks__/**', // jest pattern
-          'test.{js,jsx}', // repos with a single test file
-          'test-*.{js,jsx}', // repos with multiple top-level test files
-          '**/*{.,_}{test,spec}.{js,jsx}', // tests where the extension or filename suffix denotes that it is a test
-          '**/jest.config.js', // jest config
-          '**/jest.setup.js', // jest setup
-          '**/vue.config.js', // vue-cli config
-          '**/webpack.config.js', // webpack config
-          '**/webpack.config.*.js', // webpack config
-          '**/rollup.config.js', // rollup config
-          '**/rollup.config.*.js', // rollup config
-          '**/gulpfile.js', // gulp config
-          '**/gulpfile.*.js', // gulp config
-          '**/Gruntfile{,.js}', // grunt config
-          '**/just.config.{js,ts}', // just-task config
-          '**/protractor.conf.js', // protractor config
-          '**/protractor.conf.*.js', // protractor config
-          '**/karma.conf.js', // karma config
-        ],
+        devDependencies: false,
         optionalDependencies: false,
         peerDependencies: false,
       },
@@ -255,4 +229,48 @@ module.exports = {
       },
     ],
   },
+
+  overrides: [
+    {
+      files: [
+        // scripts
+        'build/**', // build scripts
+        'script/**', // common scripts
+        'scripts/**', // common scripts
+
+        // tests
+        'test/**', // tests
+        'tests/**', // tests
+        'spec/**', // tests
+        '**/__tests__/**', // tests
+        '**/__mocks__/**', // tests
+        'test.{js,jsx}', // repos with a single test file
+        'test-*.{js,jsx}', // repos with multiple top-level test files
+        '**/*{.,_}{test,spec}.{js,jsx}', // tests where the extension or filename suffix denotes that it is a test
+
+        // test configs
+        '**/jest.config.js', // jest config
+        '**/jest.setup.js', // jest setup
+        '**/protractor.conf.js', // protractor config
+        '**/protractor.conf.*.js', // protractor config
+        '**/karma.conf.js', // karma config
+
+        // bundler configs
+        '**/vue.config.js', // vue-cli config
+        '**/webpack.config.js', // webpack config
+        '**/webpack.config.*.js', // webpack config
+        '**/rollup.config.js', // rollup config
+        '**/rollup.config.*.js', // rollup config
+        '**/gulpfile.js', // gulp config
+        '**/gulpfile.*.js', // gulp config
+        '**/Gruntfile{,.js}', // grunt config
+        '**/just.config.{js,ts}', // just-task config
+      ]
+        .map(p => (p.startsWith('*') ? [p] : [p, `packages/*/${p}`]))
+        .flat(),
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
+  ],
 };
