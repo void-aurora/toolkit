09b2caf7fcc084af0a7e259d84dc660bcdc15a65
duduluu <duduluu@outlook.com>
2020-04-06 03:34:07 +0800

    feat(just): util exec spawn supports stdout and stderr

--- packages/just/src/utils/exec.ts
+++ packages/just/src/utils/exec.ts
@@ -1,21 +1,35 @@
 import cp from 'child_process';
 
+export interface SpawnError extends Error {
+  code?: number | null;
+}
+
 export async function spawn(
   cmd: string,
   args: readonly string[],
-  options: cp.SpawnOptions = {},
+  options: cp.SpawnOptions & {
+    stdout?: NodeJS.WritableStream;
+    stderr?: NodeJS.WritableStream;
+  } = {},
 ): Promise<void> {
   return new Promise((resolve, reject) => {
     const child = cp.spawn(cmd, args, options);
     child.on('exit', code => {
       if (code !== 0) {
-        const error = new Error(
-          `Command failed (exit code: ${code ?? 'UNKNOWN'}): ${[cmd, ...args].join(' ')}`,
+        const error: SpawnError = new Error(
+          `Command failed with exit code ${code ?? 'UNKNOWN'}: ${[cmd, ...args].join(' ')}`,
         );
+        error.code = code;
         reject(error);
         return;
       }
       resolve();
     });
+    if (options.stdout) {
+      child.stdout?.pipe(options.stdout);
+    }
+    if (options.stderr) {
+      child.stderr?.pipe(options.stderr);
+    }
   });
 }
