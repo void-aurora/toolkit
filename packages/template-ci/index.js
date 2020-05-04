b749bd174742bc90ef7f0c6d56458ac0d799dd1f
duduluu <duduluu@outlook.com>
2020-05-05 01:37:55 +0800

    chore(template-ci): change timestamp

--- packages/template-ci/index.js
+++ packages/template-ci/index.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: '@void-aurora/template-ci',
   description: 'Empty package for test ci',
-  timestamp: '2020-02-25T10:26:11.189Z',
+  timestamp: '2020-05-04T17:37:10.266Z',
 };
